e4e2e77ffa4e45e869c0637a67b55bba2552cc50
xiaokang <xunxiaokang88888@163.com>
2021-11-05 11:23:39 +0800

    j c

--- stu.sql
+++ stu.sql
@@ -4,14 +4,9 @@
 -- ------------------------------------------------------
 -- Server version	5.7.29-0ubuntu0.18.04.1
 
+print("hell world")
 
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+====================
 
 --
 -- Table structure for table `athlete`
